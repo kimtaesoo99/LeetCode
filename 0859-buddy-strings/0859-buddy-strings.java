597b566a54fb1bde5d639ecca973899ab0f6b59b
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-07-03 14:35:36 +0900

    Time: 1 ms (99.01%), Space: 41.4 MB (83.85%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        if (s.equals(goal)) {
+            int[] alpha = new int[26];
+            for (char ch : s.toCharArray()) {
+                alpha[ch - 'a']++;
+                if (alpha[ch - 'a'] == 2) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        int firstIndex = -1;
+        int secondIndex = -1;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) != goal.charAt(i)) {
+                if (firstIndex == -1) {
+                    firstIndex = i;
+                } else if (secondIndex == -1) {
+                    secondIndex = i;
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (secondIndex == -1) {
+            return false;
+        }
+        return s.charAt(firstIndex) == goal.charAt(secondIndex) && s.charAt(secondIndex) == goal.charAt(firstIndex);
+    }
+}
