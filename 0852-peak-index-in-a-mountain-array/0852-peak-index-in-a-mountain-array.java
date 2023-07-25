87cd02546b4c93aff0cf636e2691067d4bbbc0ee
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-07-25 12:27:11 +0900

    Time: 0 ms (100.00%), Space: 55.3 MB (55.22%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        int mid;
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
