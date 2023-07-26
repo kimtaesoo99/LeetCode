1d7360af6961dcc7c7a057451accec7c7231fb94
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-07-26 12:17:00 +0900

    Time: 221 ms (49.83%), Space: 59.3 MB (25.95%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int left = 1;
+        int right = 10000000;
+
+        int minSpeed = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (timeRequired(dist, mid) <= hour) {
+                minSpeed = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return minSpeed;
+    }
+
+    private double timeRequired(int[] dist, int speed) {
+        double time = 0.0;
+        for (int i = 0; i < dist.length; i++) {
+            double t = (double) dist[i] / (double) speed;
+            time += (i == dist.length - 1 ? t : Math.ceil(t));
+        }
+        return time;
+    }
+}
