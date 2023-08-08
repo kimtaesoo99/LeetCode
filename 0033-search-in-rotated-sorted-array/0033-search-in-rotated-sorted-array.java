cba86cac31338c5139fd604bd3a14a2c718d1974
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-08-08 18:34:53 +0900

    Time: 0 ms (100.00%), Space: 40.9 MB (64.16%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[n - 1]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return shiftedBinarySearch(nums, left, target);
+    }
+    
+    private int shiftedBinarySearch(int[] nums, int pivot, int target) {
+        int n = nums.length;
+        int shift = n - pivot;
+        int left = (pivot + shift) % n;
+        int right = (pivot - 1 + shift) % n;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[(mid - shift + n) % n] == target) {
+                return (mid - shift + n) % n;
+            } else if (nums[(mid - shift + n) % n] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
