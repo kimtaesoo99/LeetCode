03db39a1b82c674ee02e707e566b150193c78dd4
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-08-16 17:07:42 +0900

    Time: 40 ms (42.43%), Space: 57.1 MB (97.07%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < k; i++) {
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+        }
+        result.add(nums[dq.peekFirst()]);
+
+        for (int i = k; i < nums.length; i++) {
+            if (dq.peekFirst() == i - k) {
+                dq.pollFirst();
+            }
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+
+            dq.offerLast(i);
+            result.add(nums[dq.peekFirst()]);
+        }
+        return result.stream().mapToInt(i->i).toArray();
+    }
+}
