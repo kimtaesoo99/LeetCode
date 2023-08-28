c60d19f45b59e1cf23e07186168a81ea1902c41e
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-08-28 19:03:37 +0900

    Time: 0 ms (100.00%), Space: 40.5 MB (18.89%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+    private Queue<Integer> q = new LinkedList<>();
+
+    public MyStack() {
+    }
+
+    void push(int x) {
+        q.add(x);
+        int v = q.size() - 1;
+        int i = 0;
+        while (i < v) {
+            q.add(q.poll());
+            i++;
+        }
+    }
+
+    int pop() {
+        return q.poll();
+    }
+
+    int top() {
+        return q.peek();
+    }
+
+    boolean empty() {
+        return q.isEmpty();
+    }
+}
