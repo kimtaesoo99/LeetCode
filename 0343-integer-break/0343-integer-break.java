11fda408792ceb25422b4034255f9ba23caab30b
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-10-06 13:05:04 +0900

    Time: 0 ms (100.00%), Space: 38.8 MB (85.29%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,36 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    private int sum;
-    private List<Integer> list = new ArrayList<>();
-
     public int integerBreak(int n) {
-        combination(n);
-        return sum;
-    }
-
-    private void combination(int n) {
-        if (n == 0) {
-            getSum();
-            return;
+        if (n <= 3) {
+            return n - 1;
         }
-
-        for (int i = 1; i <= n; i++) {
-            if (list.isEmpty() || list.get(list.size() - 1) >= i) {
-                list.add(i);
-                combination(n - i);
-                list.remove(list.size() - 1);
-            }
+        
+        int result = 1;
+        while (n > 4) {
+            result *= 3;
+            n -= 3;
         }
-    }
-
-    private void getSum() {
-        if (list.size() < 2) {
-            return;
-        }
-        int result = list.stream()
-                .reduce(1, (i, j) -> i * j);
-        sum = Math.max(sum, result);
+        
+        return result * n;
     }
 }
