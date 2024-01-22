d9f0d896c1eefee15c3d6b45e68b054fc7d6eeb0
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-01-23 06:05:27 +0900

    Time: 779 ms (5.08%), Space: 45.4 MB (53.19%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int dup = -1, missing = -1;
+        int dup = -1;
+        int missing = -1;
         
         for (int i = 1; i <= nums.length; i++) {
             int count = 0;
