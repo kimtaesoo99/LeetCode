fa39a9c8708c0e4e155cb54af3bfe070e4af8bd5
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-05-08 21:08:06 +0900

    Time: 2 ms (100.00%), Space: 45.4 MB (84.78%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -0,0 +1,38 @@
+class Solution {
+    private int findMax(int[] score) {
+        int maxScore = 0;
+        for (int s : score) {
+            if (s > maxScore) {
+                maxScore = s;
+            }
+        }
+        return maxScore;
+    }
+
+    public String[] findRelativeRanks(int[] score) {
+        int N = score.length;
+
+        int M = findMax(score);
+        int[] scoreToIndex = new int[M + 1];
+        for (int i = 0; i < N; i++) {
+            scoreToIndex[score[i]] = i + 1;
+        }
+
+        final String[] MEDALS = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+
+        String[] rank = new String[N];
+        int place = 1;
+        for (int i = M; i >= 0; i--) {
+            if (scoreToIndex[i] != 0) {
+                int originalIndex = scoreToIndex[i] - 1;
+                if (place < 4) {
+                    rank[originalIndex] = MEDALS[place - 1];
+                } else {
+                    rank[originalIndex] = String.valueOf(place);
+                }
+                place++;
+            }
+        }
+        return rank;
+    }
+}
