177b4928393b96fb107eb63720f535807a111dd4
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-08-19 06:26:17 +0900

    Time: 5 ms (7.17%), Space: 41.2 MB (14.70%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+
+class Solution {
+    public int divide(int dividend, int divisor) {
+        BigInteger value = new BigInteger(String.valueOf(dividend)).divide(BigInteger.valueOf(divisor));
+        int b = value.intValue();
+        
+        if (dividend == -2147483648 && divisor == -1) {
+            return 2147483647;
+        }
+        
+        return b;
+    }
+}
