417a7a94d2216c921dfe725f4bc90c48dcae1213
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-09-05 18:50:40 +0900

    Time: 4 ms (71.80%), Space: 62.1 MB (30.82%) - LeetHub

---
+++ 2028-find-missing-observations/2028-find-missing-observations.java
@@ -0,0 +1,21 @@
+public class Solution {
+
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        int sum = 0;
+        for (int roll : rolls) {
+            sum = sum + roll;
+        }
+        int remainingSum = mean * (n + rolls.length) - sum;
+        if (remainingSum > 6 * n || remainingSum < n) {
+            return new int[0];
+        }
+        int distributeMean = remainingSum / n;
+        int mod = remainingSum % n;
+        int[] nElements = new int[n];
+        Arrays.fill(nElements, distributeMean);
+        for (int i = 0; i < mod; i++) {
+            nElements[i]++;
+        }
+        return nElements;
+    }
+}
