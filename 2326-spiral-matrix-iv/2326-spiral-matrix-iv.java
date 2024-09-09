3bef35bf0b7b2724b6cdbf50dadff6b7a7bc4569
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-09-09 11:49:08 +0900

    Time: 13 ms (9.33%), Space: 56.2 MB (98.31%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+        int i = 0, j = 0, cur_d = 0, movement[][] = {
+            { 0, 1 },
+            { 1, 0 },
+            { 0, -1 },
+            { -1, 0 },
+        };
+        int[][] res = new int[m][n];
+        for (int[] row : res) {
+            Arrays.fill(row, -1);
+        }
+
+        while (head != null) {
+            res[i][j] = head.val;
+            int newi = i + movement[cur_d][0], newj = j + movement[cur_d][1];
+
+        
+            if (
+                Math.min(newi, newj) < 0 ||
+                newi >= m ||
+                newj >= n ||
+                res[newi][newj] != -1
+            ) cur_d = (cur_d + 1) % 4;
+            i += movement[cur_d][0];
+            j += movement[cur_d][1];
+
+            head = head.next;
+        }
+
+        return res;
+    }
+}
