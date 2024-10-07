b9a39948796ef8ba7a8ab84631cdd47f881dc233
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-10-07 22:10:07 +0900

    Time: 9 ms (17.26%), Space: 44.3 MB (28.60%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    public int minLength(String s) {
+        while (s.contains("AB") || s.contains("CD")) {
+            if (s.contains("AB")) {
+                s = s.replace("AB", "");
+            } else if (s.contains("CD")) {
+                s = s.replace("CD", "");
+            }
+        }
+
+        return s.length();
+    }
+}
