a1199aa8e90463882c04cd13bda0ecc5204212f9
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-10-15 18:14:18 +0900

    Time: 7 ms (100.00%), Space: 45.4 MB (54.12%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    public long minimumSteps(String s) {
+        int whitePosition = 0;
+        long totalSwaps = 0;
+
+        for (int currentPos = 0; currentPos < s.length(); currentPos++) {
+            if (s.charAt(currentPos) == '0') {
+                totalSwaps += currentPos - whitePosition;
+
+                whitePosition++;
+            }
+        }
+
+        return totalSwaps;
+    }
+}
